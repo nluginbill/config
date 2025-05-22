4dc93d0f83706c8ab8ec40dc973c1bc6d89ff8d9
Nathaniel Luginbill <nathanielluginbill@Nathaniels-MacBook-Pro.local>
2025-05-22 15:34:36 -0500

    latest

--- dot_config/exact_nvim/lua/config/options.lua
+++ dot_config/exact_nvim/lua/config/options.lua
@@ -24,7 +24,7 @@ vim.opt.sessionoptions:remove("blank")
 vim.opt.sessionoptions:remove("help")
 vim.opt.sessionoptions:remove("skiprtp")
 -- vim.opt.sessionoptions:remove("curdir")
-vim.opt.sessionoptions:append("localoptions")
+-- vim.opt.sessionoptions:append("localoptions")
 -- NOTE: adding options can result in unexpected behavior, e.g. if you make nvim config changes and load an existing session, those changes may not be integrated.
 -- vim.opt.sessionoptions:append("options")
 -- Change dir to currently open buffer
